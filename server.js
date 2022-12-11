29858ffef535ea214ecd7a70f2a8d5f82905706c
vagif-guliyev <vaguliyev@gmail.com>
2022-12-11 10:20:47 +0400

    Add http server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running at ${PORT}`));
