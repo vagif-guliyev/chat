0ebd229a6165027ce694a9e83f3a05f9ee225935
vagif-guliyev <vaguliyev@gmail.com>
2022-12-11 10:24:01 +0400

    Set the socketio up

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const socketio = require('socketio');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket => {
+    console.log('New Connection established');
+});
 
 const PORT = 3000 || process.env.PORT;
 
